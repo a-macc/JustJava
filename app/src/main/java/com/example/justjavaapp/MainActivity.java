684b443559d8ef9ade8ac533296e036ac95acb5c
Anurag Makade <iit2018121@iiita.ac.in>
2019-03-11 23:47:47 +0530

    Add options for name and toppings

--- app/src/main/java/com/example/justjavaapp/MainActivity.java
+++ app/src/main/java/com/example/justjavaapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.justjavaapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.justjavaapp.R;
-
-import java.text.NumberFormat;
+//import com.example.justjavaapp.R;
+//
+//import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,43 +21,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 1;
+    int quantity = 2;
 
     public void increment(View view) {
+        if(quantity == 100) {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot have more than 100 coffees";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+            return;
+        }
         quantity++;
         display(quantity);
     }
 
     public void decrement(View view) {
+        if(quantity == 1) {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot have less than 1 coffee";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+            return;
+        }
         quantity--;
         display(quantity);
     }
 
     public void submitOrder(View view) {
-
+        EditText editText = findViewById(R.id.name_field);
+        String name = editText.getText().toString();
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
         int price = calculatePrice(quantity);
-        String priceMessage = calculateOrderSummary(price);
+        String priceMessage = calculateOrderSummary(price, hasWhippedCream, hasChocolate, name);
         displayMessage(priceMessage);
     }
 
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.format("%s", number));
     }
 
     private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
     }
 
     private int calculatePrice(int quantity) {
-        int price = quantity * 10;
-        return price;
+        int price = 10;
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        if(hasWhippedCream)
+            price += 2;
+
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        if(hasChocolate)
+            price += 4;
+
+        return price*quantity;
     }
 
-    private String calculateOrderSummary(int price) {
-        String orderSummary = "Name: Kaptain Kunal\nQuantity: " + price / 10 + "\nPrice: ₹" + price +
-                "\nThank You!";
+    private String calculateOrderSummary(int price, boolean addWippedCream, boolean addChocolate, String name) {
+        String orderSummary = "Name: " + name;
+        orderSummary += "\nAdd Whipped Cream? " + addWippedCream;
+        orderSummary += "\nAdd Chocolate? " + addChocolate;
+        orderSummary += "\nQuantity: " + quantity;
+        orderSummary += "\nPrice: ₹" + price;
+        orderSummary += "\nThank You!";
         return orderSummary;
 
     }
